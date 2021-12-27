9053f5432051263a45b60ec2d06e05f9b85247c0
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2021-12-26 16:32:07 -0800

    Update Q4.sql

--- HW2/Q4.sql
+++ HW2/Q4.sql
@@ -14,15 +14,8 @@ select count(test_id) as Number_of_tests from test where test_result='positive' 
 
 --Number of Employees who self reported symptoms
 
-select count(emp_id) as number_of_employees_self_reported_symptoms from symptom where reported_date between '05-SEP-2021' and '10-SEP-2021';
+select count(*) from ( select distinct(emp_id) as number_of_employees_self_reported_symptoms from symptom where reported_date between '05-SEP-2021' and '10-SEP-2021' group by emp_id);
 
 --Number of positive cases 
 
 select count(test_result) as number_of_positive_cases from test where test_result='positive' and test_date between '05-SEP-2021' and '10-SEP-2021';
-
-
-/*
-
-Note: The data inserted in tables date ranges from 01-SEP-2021 to 30-SEP-2021 only.
-
-*/
